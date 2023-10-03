008a9de96fddf4f5319671145310de5ac48648b4
albertcastineira <albertcastiaran@gmail.com>
2023-10-03 17:46:47 +0200

    fixed pokemon card id position

--- components/PokemonList/PokemonCard.tsx
+++ components/PokemonList/PokemonCard.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 export default function PokemonCard({pokemonName, imageUrl, pokemonId} : any) {
   return (
-    <div className="pokemonCard bg-white rounded-sm p-1 text-center">
+    <div className="pokemonCard bg-white rounded-md p-1 text-center">
+        <p className="text-gray-300 font-semibold text-2xl m-1 absolute">#{pokemonId}</p>
         <img className="w-[150px] m-[auto] drop-shadow-2xl" src={imageUrl} alt="" />
-        <span className="float-right pr-2 font-semibold text-gray-300 my-2">#{pokemonId}</span>
         <p className="font-bold text-black capitalize text-center my-2">{pokemonName}</p>
     </div>
   )
