e312edee3038fb3cd63058dde5d6368064d5281d
Albert Castiñeira Aranda <albertcastiaran@gmail.com>
2023-10-06 01:36:35 +0200

    now pokemon card displays type with icons

--- components/PokemonList/PokemonCard.tsx
+++ components/PokemonList/PokemonCard.tsx
@@ -4,17 +4,25 @@ import Image from "next/image"
 export default function PokemonCard({ pokemonName, imageUrl, pokemonId, showPokemon, pokemonTypes }: any) {
   return (
     <div className="pokemonCard shadow-md bg-gradient-to-br from-white to-gray-100 rounded-md p-1 text-center">
-      <p className="text-gray-300 font-semibold text-2xl m-1 absolute">
-        #{pokemonId}
-      </p>
-      <p className="text-black">
+      <div className="grid grid-cols-2">
+        <div className="text-start">
+        <p className="text-gray-300 font-semibold text-2xl m-1 absolute">
+          #{pokemonId}
+        </p>
+        </div>
+        <div className="text-end mr-2">
         {
-          pokemonTypes.map((type: any) => <Image className="inline-block ml-2 mt-2 typeIcon" height={20} width={20} src={`typeIcons/${type}.svg`} alt='' />)
+            pokemonTypes.map((type: any) => 
+              <Image key={pokemonId} className={`inline-block ml-2 mt-2 p-1.5 my-2 rounded-md ${type}`} height={28} width={28} src={`typeIcons/${type}.svg`} alt='' />
+            )
         }
-      </p>
-      <img
-        className="pokemonImg w-[150px] m-[auto] drop-shadow-2xl"
+        </div>
+      </div>
+      <Image
+        className="pokemonImg m-[auto] drop-shadow-2xl"
         src={imageUrl}
+        width={150}
+        height={150}
         alt=""
         onClick={() => showPokemon()} // Call showPokemon when the image is clicked
       />
